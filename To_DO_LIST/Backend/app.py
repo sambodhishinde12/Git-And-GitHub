bf5863bc588dd034e591e53d31b1fdc357bae4bf
Ubuntu <ubuntu@ip-172-31-33-50.ap-south-1.compute.internal>
2026-02-23 15:25:47 +0000

    added host

--- To_DO_LIST/Backend/app.py
+++ To_DO_LIST/Backend/app.py
@@ -26,4 +26,4 @@ def submit_todo_item():
     return jsonify({"message": "To-Do item saved successfully!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
